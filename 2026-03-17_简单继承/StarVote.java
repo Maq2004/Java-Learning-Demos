87c0374b5bbf6928961a857ddfbdccff72421ea7
Maq2004 <1503583528@qq.com>
2026-03-17 20:31:14 +0800

    简单继承学习

---
+++ 2026-03-17_简单继承/StarVote.java
@@ -0,0 +1,13 @@
+package day_09;
+
+public class StarVote extends Vote {
+	
+	private String name = "周杰伦";
+	
+	public void showStar() {
+		
+		System.out.println("当前正在为：" + name +" 投票" );
+		
+	}
+
+}

9c9f8c0c38c76334fa3ab9d5612f7248b45c8c15
马谦 <1503583528@qq.com>
2026-04-09 21:18:01 +0800

    线程和进程学习

---
+++ 2026-04-09_线程/day_31/MyThread_02.java
@@ -0,0 +1,55 @@
+package day_31;
+
+public class MyThread_02 implements Runnable {
+	
+	public void t1() {
+		
+
+			for( int i = 0 ;  i < 10 ; i++) {
+				synchronized ( this ) {				
+				System.out.print(Thread.currentThread().getName()+"=");
+				
+				System.out.print("微");
+				System.out.print("冷");
+				System.out.print("的");
+				System.out.print("雨");
+				System.out.println();
+			}
+				// 由于性能太好，导致线程A输出一条，线程B输出一条的理想输出无法做到，就让抢进来的线程睡1毫秒，让另一个线程来
+				try {
+					Thread.sleep(1);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				
+		}
+	
+	}
+	
+	public  void t2() {
+		for( int i = 0 ;  i < 10 ; i++) {
+			synchronized ( this ) {	
+			System.out.print(Thread.currentThread().getName()+"=");
+			
+			System.out.print("狂");
+			System.out.print("热");
+			System.out.print("的");
+			System.out.print("血");
+			System.out.println();
+			}
+		}
+	}
+	
+	@Override
+	public void run() {
+		
+		t1();
+		t2();
+		
+
+		
+		
+	}
+
+
+}
